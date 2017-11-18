bc07da12ee5db11f25e4307f0ed586979ed8f215
TJ Hillard <tjhillard@gmail.com>
2017-11-18 14:59:34 -0800

    Update url in manifest to https

--- app.js
+++ app.js
@@ -23,19 +23,19 @@ var checkForIsPlaying = setInterval(function() {
     var player = document.querySelector('.player-basic.is-playing');
     if (player) {
         clearInterval(checkForIsPlaying);
-
+        var audioElement = document.querySelector('audio');
         player.appendChild(controls);
 
         if (savedVolume) {
             controls.value = savedVolume * 100;
-            document.querySelector('audio').volume = savedVolume;
+            audioElement.volume = savedVolume;
         } else {
-            document.querySelector('audio').volume = 0.8;
+            audioElement.volume = 0.8;
         }
 
         controls.addEventListener('change', function(e) {
             var volume = e.target.value / 100;
-            document.querySelector('audio').volume = volume;
+            audioElement.volume = volume;
             chrome.storage.sync.set({'nprVolume': volume});
         });
     }
